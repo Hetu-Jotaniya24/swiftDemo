ecf952d63949e17f25a94840b5a3b6141ac8fa19
Hiral Jotaniya <46888674+Hetu-Jotaniya24@users.noreply.github.com>
2021-11-09 13:51:09 +0530

    Add files via upload

---
+++ CoreDataSwiftUIMVVM-main/CoreDataMVVM/ContentView.swift
@@ -0,0 +1,53 @@
+//
+//  ContentView.swift
+//  CoreDataMVVM
+//
+//  Created by Hiral Jotaniya on 4/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @StateObject private var taskListVM = TaskListViewModel()
+    
+    func deleteTask(at offsets: IndexSet) {
+        offsets.forEach { index in
+            let task = taskListVM.tasks[index]
+            taskListVM.delete(task)
+        }
+        
+        taskListVM.getAllTasks()
+    }
+    
+    var body: some View {
+        VStack {
+            HStack {
+                TextField("Enter task name", text: $taskListVM.title)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                Button("Save") {
+                    taskListVM.save()
+                    taskListVM.getAllTasks()
+                }
+            }
+            
+            List {
+                ForEach(taskListVM.tasks, id: \.id) { task in
+                    Text(task.title)
+                }.onDelete(perform: deleteTask)
+            }
+            
+            Spacer()
+        }.padding()
+        .onAppear(perform: {
+            taskListVM.getAllTasks()
+        })
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
